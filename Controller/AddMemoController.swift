c89b11d0b1a428cf29220addf78f24248764a762
dlrjswns <leekyunjun@gmail.com>
2021-12-12 20:59:54 +0900

    0.0v 버튼 동작 및 Model을 이용한 삽입 UIDatePicker에 대한 UX ***

---
+++ Controller/AddMemoController.swift
@@ -0,0 +1,127 @@
+//
+//  AddMemoController.swift
+//  CL4_ToDoTDD
+//
+//  Created by 이건준 on 2021/12/12.
+//
+
+import UIKit
+
+class AddMemoController:UIViewController{
+    //MARK: -UI Component
+    private lazy var titleTextFd:UITextField={
+        let fd = UITextField()
+        fd.placeholder = "Title"
+        fd.layer.borderWidth = 1
+        fd.layer.borderColor = UIColor.systemGray.cgColor
+        fd.layer.cornerRadius = 10
+        fd.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        fd.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        return fd
+    }()
+    
+    private lazy var addressTextFd:UITextField={
+        let fd = UITextField()
+        fd.placeholder = "Address"
+        fd.layer.borderWidth = 1
+        fd.layer.borderColor = UIColor.systemGray.cgColor
+        fd.layer.cornerRadius = 10
+        fd.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        fd.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        return fd
+    }()
+    
+    private lazy var dateLabel:UILabel={
+       let lb = UILabel()
+        lb.text = "Date"
+        return lb
+    }()
+    
+    private lazy var datePicker:UIDatePicker={
+       let pk = UIDatePicker()
+        pk.datePickerMode = .date
+        pk.minimumDate = Date() //현재 날짜를 최소날짜로 지정
+        return pk
+    }()
+    
+    private lazy var cancelButton:UIButton={
+        let btn = UIButton(type: UIButton.ButtonType.system)
+        btn.setTitle("cancel" , for: UIControl.State.normal)
+        btn.tintColor = .systemRed
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        return btn
+    }()
+    
+    private lazy var saveButton:UIButton={
+        let btn = UIButton(type: UIButton.ButtonType.system)
+        btn.setTitle("save", for: UIControl.State.normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        btn.addTarget(self, action: #selector(tapSaveButton), for: UIControl.Event.touchUpInside)
+        return btn
+    }()
+    
+//    private lazy var da1tePicker:UIPickerView = {
+//       let pk = UIPickerView()
+//
+//        return pk
+//    }()
+    
+    //MARK: -For Setting
+//    func setdatePicker(){
+//        da1tePicker.delegate = self
+//        da1tePicker.dataSource = self
+//    }
+    
+    //MARK: -Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+    }
+    
+    //MARK: -Objc
+    @objc func tapSaveButton(){
+        let vc = TodoListController()
+        vc.memoArray.append(Memo(title: titleTextFd.text!, comment: addressTextFd.text!, date: datePicker.description))
+        self.navigationController?.popToViewController(vc, animated: true)
+        print("dfds")
+    }
+    
+    //MARK: -Configure
+    func configure(){
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(titleTextFd)
+        titleTextFd.translatesAutoresizingMaskIntoConstraints = false
+        titleTextFd.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleTextFd.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        
+        view.addSubview(addressTextFd)
+        addressTextFd.translatesAutoresizingMaskIntoConstraints = false
+        addressTextFd.centerXAnchor.constraint(equalTo: titleTextFd.centerXAnchor).isActive = true
+        addressTextFd.topAnchor.constraint(equalTo: titleTextFd.bottomAnchor, constant: 30).isActive = true
+        
+        view.addSubview(dateLabel)
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.centerXAnchor.constraint(equalTo: addressTextFd.centerXAnchor).isActive = true
+        dateLabel.topAnchor.constraint(equalTo: addressTextFd.bottomAnchor, constant: 40).isActive = true
+    
+        view.addSubview(datePicker)
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.centerXAnchor.constraint(equalTo: dateLabel.centerXAnchor).isActive = true
+        datePicker.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 10).isActive = true
+        
+        
+        view.addSubview(cancelButton)
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        cancelButton.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: 50).isActive = true
+        cancelButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 80).isActive = true
+        
+        view.addSubview(saveButton)
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        saveButton.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: 50).isActive = true
+        saveButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -80).isActive = true
+    }
+}
+
+
